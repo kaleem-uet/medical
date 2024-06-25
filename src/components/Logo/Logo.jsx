8917c9ff6e0b5ad55c1822249dd4553bb2217129
shreddeduniongit <shreddedunion114@gmail.com>
2024-06-25 15:07:08 +0500

    logo updated

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,6 @@
 // material-ui
 import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography'
-
 /**
  * if you want to use image instead of <svg> uncomment following.
  *
@@ -25,7 +24,7 @@ const Logo = () => {
      */
     <>
 
-      <Typography variant="body1" color="initial" fontWeight="bold" sx={{fontSize: "23px"}}>Logo</Typography>
+      <Typography variant="body1" color="initial" fontWeight="bold" sx={{fontSize: "23px"}}> Medical System</Typography>
 
       {/* <svg width="118" height="35" viewBox="0 0 118 35" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
