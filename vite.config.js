bab0216f02cb4670bfaa45e71555a6c79968b852
Kaleem Ullah <71625133+kaleem-uet@users.noreply.github.com>
2024-06-26 16:32:24 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 3000
-  }
-});
+})
+
