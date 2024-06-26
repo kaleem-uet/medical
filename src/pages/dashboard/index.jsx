c463b9c2a09838fe7201b47ae24bc04cd46997d5
kaleem-uet <kaleemullahk989@gmail.com>
2024-06-26 15:21:21 +0500

    comment remove

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -20,28 +20,6 @@ const DashboardDefault = () => {
   const { drawerOpen } = useSelector((state) => state.menu);
   console.log("detailsNode", detailsNode);
   return (
-    // <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-    //   {/* row 1 */}
-    //   <Grid item xs={12} sx={{ mb: -2.25 }}>
-    //     <Typography variant="h5">Dashboard</Typography>
-    //   </Grid>
-    //   <Grid item xs={12} sm={6} md={4} lg={3}>
-    //     <AnalyticEcommerce title="Total Page Views" count="4,42,236" percentage={59.3} extra="35,000" />
-    //   </Grid>
-    //   <Grid item xs={12} sm={6} md={4} lg={3}>
-    //     <AnalyticEcommerce title="Total Users" count="78,250" percentage={70.5} extra="8,900" />
-    //   </Grid>
-    //   <Grid item xs={12} sm={6} md={4} lg={3}>
-    //     <AnalyticEcommerce title="Total Order" count="18,800" percentage={27.4} isLoss color="warning" extra="1,943" />
-    //   </Grid>
-    //   <Grid item xs={12} sm={6} md={4} lg={3}>
-    //     <AnalyticEcommerce title="Total Sales" count="$35,078" percentage={27.4} isLoss color="warning" extra="$20,395" />
-    //   </Grid>
-    //  <Grid item md={12}>
-
-    //  </Grid>
-
-    // </Grid>
     <>
       <Box sx={{ height: "70vh", width: `${drawerOpen} ?  '82vw' : '100vw'`,position:'relative' }}>
         <MyResponsiveCirclePacking
@@ -57,7 +35,7 @@ const DashboardDefault = () => {
       </Box>
       {showDetails && (
         <Box sx={{position:"absolute",top:70,right:1}}>
- <AnalyticEcommerce
+       <AnalyticEcommerce
         title={detailsNode.color}
         count={detailsNode.data.name}
         percentage={27.4}
